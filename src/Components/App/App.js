ce84e9395b9b446f59b440d81ec6774aeeb29902
Ilyas Abduljabar <ilyassabdul94@gmail.com>
2019-11-27 18:42:58 +0800

    adding the search result and tracklist components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
-function App() {
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state= {
+      SearchResults:[{name:'', artist:'', album:'', id:''}]
+    }
+  }
+
+  render () {
   return (
     <div>
     <h1>Ja<span className="highlight">mmm</span>ing</h1>
     <div className="App">
-      // Add a SearchBar component //
+      <SearchBar />
       <div className="App-playlist">
-        // Add a SearchResults component//
-      //   Add a Playlist component    //
+       <SearchResults  searchResults={this.state.SearchResults} />
+       <Playlist />
       </div>
     </div>
   </div>
   );
+  
+  }
 }
 
 export default App;
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -7,8 +7,7 @@ class Playlist extends React.Component {
         return (
             <div className="Playlist">
             <input value="New Playlist"/>
-            <!-- Add a TrackList component -->
-            <button className="Playlist-save">SAVE TO SPOTIFY</button>
+\            <button className="Playlist-save">SAVE TO SPOTIFY</button>
           </div>
         )
     }
